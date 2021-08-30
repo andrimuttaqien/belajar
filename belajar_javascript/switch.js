a6eced054e62c509fc59b8158ba8007f7b33e477
andrimuttaqien <andrineverstoptorun@gmail.com>
2021-08-30 15:23:40 +0700

    Update

--- belajar_javascript/switch.js
+++ belajar_javascript/switch.js
@@ -1,9 +1,9 @@
-var angka   =parseInt(prompt("Masukan Angka : \n (harus angka)"));
-switch(angka) {
-    case 1:
-        alert("Anda Memasukan Angkat " +angka);
-        break;
-    default:
-        alert("Anda Memasukan Angka");
-        break;
-}
+// var angka   =parseInt(prompt("Masukan Angka : \n (harus angka)"));
+// switch(angka) {
+//     case 1:
+//         alert("Anda Memasukan Angkat " +angka);
+//         break;
+//     default:
+//         alert("Anda Tidak Memasukan Angka");
+//         break;
+// }
