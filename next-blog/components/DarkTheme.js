3d3cf05a07bf6636669ef7ad36305f246ff51d19
andrimuttaqien <andrineverstoptorun@gmail.com>
2021-11-04 16:29:49 +0700

    update

---
+++ next-blog/components/DarkTheme.js
@@ -0,0 +1,15 @@
+function DarkTheme() {
+  return (
+    <style jsx global>
+      {`
+        /* Dark Theme */
+        :root {
+          --background-color: black;
+          --link-color: yellow;
+          --text-color: white;
+        }
+      `}
+    </style>
+  );
+}
+export default DarkTheme;
---
+++ next-blog/components/ThemeSwitch.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import DarkTheme from "./DarkTheme";
+
+function loadDarkMode() {
+  if (typeof localStorage === "undefined") {
+    return false;
+  }
+  const value = localStorage.getItem("darkMode");
+  return value === null ? false : JSON.parse(value);
+}
+
+function ThemeSwitch() {
+  const [darkMode, setDarkMode] = useState(loadDarkMode);
+
+  const handleOnClick = () => {
+    localStorage.setItem("darkMode", JSON.stringify(!darkMode));
+    setDarkMode(!darkMode);
+  };
+
+  console.log("[ThemeSwitch] darkMode :", darkMode);
+  const text = darkMode ? "Light Mode" : "Dark Mode";
+  return (
+    <>
+      <button onClick={handleOnClick} suppressHydrationWarning>
+        {text}
+      </button>
+      <style jsx>
+        {`
+          button {
+            background: none;
+            color: inherit;
+            border: none;
+            cursor: pointer;
+          }
+        `}
+      </style>
+      {darkMode && <DarkTheme />}
+    </>
+  );
+}
+export default ThemeSwitch;
